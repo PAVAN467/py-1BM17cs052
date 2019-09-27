45a2326639911dfbbd8010c20f9b05633aa3bdf7
PAVAN467 <53818247+PAVAN467@users.noreply.github.com>
2019-09-27 14:13:58 +0530

    Update 6_brackets.py

--- 6_brackets.py
+++ 6_brackets.py
@@ -1,20 +1,31 @@
-stack=[]
 import sys
-o=['(','[','{']
-c=[')',']','}']
-s=input()
-for i in s:
-	#print(stack)
-	if i in o :
-		stack.append(i)
-	elif i in c :
-		ch=stack[len(stack)-1]
-		ch1=c[o.index(ch)]
-		if ch1==i : 
-			stack.pop()
-		else : 
-			print("invalid")
-			exit()
-			
+class validity :
+  def __init__(self,stack,o,c,s) :
+    self.stack=stack
+    self.o=o
+    self.c=c
+    self.s=s
+  def check(self) :
+    for i in self.s:
+      if i in self.o :
+        self.stack.append(i)
+      elif i in self.c :
+        ch=self.stack[len(self.stack)-1]
+        ch1=self.c[self.o.index(ch)]
+        if ch1==i : 
+          self.stack.pop()
+        else : 
+          print("invalid")
+          sys.exit()
 
-if len(stack)==0 : print("valid")
+
+
+s=input()
+stack=[]		
+o=['(','[','{']
+c=[')',']','}']	
+
+obj=validity(stack,o,c,s)
+obj.check()
+
+if len(obj.stack)==0 : print("validate")
